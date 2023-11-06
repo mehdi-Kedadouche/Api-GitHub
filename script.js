85892ad3df9721a8e5a1e3462ad1b30610236c0b
Mehdi Kedadouche <mehdi.kedadouche@hotmail.fr>
2023-11-06 23:20:45 +0100

    Votre message de commit

--- script.js
+++ script.js
@@ -8,40 +8,81 @@ const input = document.getElementById("input");
 console.log(input);
 
 btn.onclick = async () => {
+
   try {
     const response = await fetch("https://api.github.com/users/" + input.value);
     const data = await response.json();
-console.table(data);
-if(data !== false){
-output.textContent = "";   
-const name = document.createElement("h2");
-name.textContent = `Compte de ${data.name}`;
-output.appendChild(name);
 
-const location = document.createElement("p");
-location.textContent = `Localisation : ${data.location}`;
-output.appendChild(location);
+    if (data !== false) {
 
-const bio = document.createElement("p");
-bio.textContent = `Biographie : ${data.bio}`;
-output.appendChild(bio);
-   
-  
-    const img = document.createElement("img");
-    img.src = data.avatar_url;
-    img.style.width = "130px";
-    
+      console.log(data);
 
-    output.appendChild(img);
+      output.textContent = "";
 
-  }else {  
-    const erreur = document.createElement("p");
-    erreur.textContent = `Le compte n'exitste pas`;
-    output.appendChild(erreur); 
-  }
+      const name = document.createElement("h2");
+      name.textContent = `Compte de ${data.name}`;
+      output.appendChild(name);
+
+      const img = document.createElement("img");
+      img.src = data.avatar_url;
+      img.style.width = "150px";
+      output.appendChild(img);
+
+      const location = document.createElement("p");
+      location.textContent = `Localisation : ${data.location}`;
+      output.appendChild(location);
+
+      const bio = document.createElement("p");
+      bio.textContent = `Biographie : ${data.bio}`;
+      output.appendChild(bio);
+
+      const company = document.createElement("p");
+      company.textContent = `company : ${data.company}`;
+      output.appendChild(company);
+
+      output.style.display = 'block';
+
+
+    } else {
+      const erreur = document.createElement("p");
+      erreur.textContent = `Le compte n'exitste pas`;
+      output.appendChild(erreur);
+    }
 
   } catch (error) {
     console.log("Une erreur s'est produite :", error);
-   
+
   }
 };
+
+input.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    output.textContent = "";
+
+    const name = document.createElement("h2");
+    name.textContent = `Compte de ${data.name}`;
+    output.appendChild(name);
+
+    const img = document.createElement("img");
+    img.src = data.avatar_url;
+    img.style.width = "150px";
+    output.appendChild(img);
+
+    const location = document.createElement("p");
+    location.textContent = `Localisation : ${data.location}`;
+    output.appendChild(location);
+
+    const bio = document.createElement("p");
+    bio.textContent = `Biographie : ${data.bio}`;
+    output.appendChild(bio);
+
+    const company = document.createElement("p");
+    company.textContent = `company : ${data.company}`;
+    output.appendChild(company);
+
+    output.style.display = 'block';
+  }
+});
+
+// Gestionnaire d'événements pour le bouton
+btn.onclick = processInput;
